f0cfc05049d676b5fd1cfdf10d7919ffd2c2695d
Emmanuel Okafor <54027226+Manny2x@users.noreply.github.com>
2024-04-16 10:15:04 +0000

    Update ex_2

--- exercises/exercise_2.py
+++ exercises/exercise_2.py
@@ -1,2 +1,11 @@
 # Exercise 2
 # Your solution comes here
+
+# Read the input number from the user
+number = int(input("Enter a natural number: "))
+
+# Get ten place digit using integer division and modulo
+next_even_number = number + 1
+
+# Print the result using f-string
+print(next_even_number)
