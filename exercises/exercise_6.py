ebb2c384bd5672b6e9174b81e888e3b0bbb6dfc3
Emmanuel Okafor <54027226+Manny2x@users.noreply.github.com>
2024-05-01 09:56:46 +0000

    Completed up to exercise 9

--- exercises/exercise_6.py
+++ exercises/exercise_6.py
@@ -1,2 +1,9 @@
 # Exercise 6
 # Your solution comes here
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 % num2 == 0:
+        print("YES")
+else:
+        print("NO")
--- exercises/exercise_9.py
+++ exercises/exercise_9.py
@@ -1,2 +1,10 @@
 # Exercise 9
 # Your solution comes here
+incre = 360 / 12
+val1 = int(input()) * 60
+val2 = int(input()) 
+val3 = int(input()) / 60
+total = val1 + val2 + val3
+angle = 0.5 * total
+
+print(angle)
